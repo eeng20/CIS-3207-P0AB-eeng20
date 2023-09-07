d46e41921e4c90224899e18e7476a1edda5c90ee
Estelee Eng <tul40997@temple.edu>
2023-09-07 11:33:12 -0400

    deleting main parameter

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /* testing code ls with main */
 
 #include <stdio.h>
-#include <stdlib.h.>
+#include <stdlib.h>
 #include <time.h>
 #include "tuls.c"
 
 int main(int argc, char const *argc[]) {
-	tuls();
+	tuls("CIS-3207-P0AB-eeng20/");
 	return(0);
 }
 
--- tuls.c
+++ tuls.c
@@ -1,19 +1,26 @@
 /* code for tuls */
-
+#define _DEFAULT_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>				// recur & visit directories
 #include <sys/stat.h>			// determine what a file object is
 
-
-
-int tuls(directory) {
-
+int tuls(char path) {
 	struct dirent **namelist;
-	int n; 
-	n=scandir(directory,&namelist,NULL,alphasort);
-	
-	exit(0);
-	// printf("INVALID ARGUEMENT"); 
-	// exit(NON_ZERO_INT);
+           int n;
+           char *path
+
+           n = scandir(path, &namelist, NULL, alphasort);
+           if (n == -1) {
+               perror("scandir");
+               exit(EXIT_FAILURE);
+           }
+
+           while (n--) {
+               printf("%s\n", namelist[n]->d_name);
+               free(namelist[n]);
+           }
+           free(namelist);
+
+           exit(EXIT_SUCCESS);
 }
