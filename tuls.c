70f60bc5ef3fe9f164e7de8dbf455a8d09233d08
Estelee Eng <tul40997@temple.edu>
2023-09-10 09:03:51 -0400

    recursive portion 1

--- tuls.c
+++ tuls.c
@@ -5,22 +5,35 @@
 #include <dirent.h>				// recur & visit directories
 #include <sys/stat.h>			// determine what a file object is
 
-int tuls(char *path) {
-	struct dirent **namelist;
-	int n;
+int tuls(int argc, char const *argv[]) {
+    struct dirent **namelist;
+    int n;
+	if (argc != 0) {
+        n = scandir(argv[0], &namelist, NULL, alphasort);
+        if (n == -1) {
+            perror("scandir");
+            exit(EXIT_FAILURE);
+        }
 
-	n = scandir(path, &namelist, NULL, alphasort);
-	if (n == -1) {
-		perror("scandir");
-		exit(EXIT_FAILURE);
-	}
+        else {
+            for (int i = 0; i < n; i++) {
+                    printf("%s\n", namelist[i]->d_name);
+                    free(namelist[i]);
+                }
+        }
+    }
+    n = scandir(".", &namelist, NULL, alphasort);
+    if (n == -1) {
+        perror("scandir");
+        exit(EXIT_FAILURE);
+    }
 
     else {
-        for (i = 0; i < n; i++) {
-                printf("%s\n", namelist[i]->d_name);
-                free(namelist[i]);
-                }
+        for (int i = 0; i < n; i++) {
+            printf("%s\n", namelist[i]->d_name);
+            }
             }
     free(namelist);
     exit(EXIT_SUCCESS);
 }
+
